ec6366679d55068ab52e1b6ac612584150ce5f5b
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-02-04 10:05:48 -0500

    travis

--- tests/testLike.py
+++ tests/testLike.py
@@ -11,12 +11,14 @@ class LikeTests(unittest.TestCase):
         ans = lhood.model(vec,3,4)
         self.assertEqual(model_true.tolist(), ans.tolist())
 
-    # def test_lnLike(self):
-    #     model = np.loadtxt("tests/data/testTheory.txt")
-    #     data = np.loadtxt("tests/data/testData.txt")
-    #     vec = np.linspace(10.,20.,5)
-    #     ans = lhood.model(vec,3,4)
-    #     self.assertEqual(model_true.tolist(), ans.tolist())
+    def test_lnLike(self):
+        model = np.loadtxt("tests/data/testTheory.txt")
+        data = np.loadtxt("tests/data/testData.txt")
+        invc = np.loadtxt("tests/data/testInv.txt")
+        vec = np.linspace(10.,20.,5)
+        ans = lhood.model(vec,3,4)
+        print ans
+        #self.assertEqual(model_true.tolist(), ans.tolist())
         
 
 if __name__ == '__main__':
