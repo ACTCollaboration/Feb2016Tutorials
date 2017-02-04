f3e6f2f798f7ce26f4321f1eff901872922b783e
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-02-04 09:56:52 -0500

    setup file

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup, Extension
+import os
+
+
+
+setup(name='actExamples',
+      version='0.1',
+      description='ACT Examples',
+      author='Mathew Madhavacheril',
+      author_email='mathewsyriac@gmail.com',
+      license='BSD-2-Clause',
+      packages=['actExamples'],
+      zip_safe=False)
