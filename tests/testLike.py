c8f7a44a4edf0e652831ad1d16eed571ea06fbaa
Mathew Madhavacheril <mathewsyriac@gmail.com>
2017-02-04 10:09:34 -0500

    testing log like

--- tests/testLike.py
+++ tests/testLike.py
@@ -16,9 +16,9 @@ class LikeTests(unittest.TestCase):
         data = np.loadtxt("tests/data/testData.txt")
         invc = np.loadtxt("tests/data/testInv.txt")
         vec = np.linspace(10.,20.,5)
-        ans = lhood.model(vec,3,4)
-        print ans
-        #self.assertEqual(model_true.tolist(), ans.tolist())
+        ans = lhood.logLike(model,data,invc)
+        expected = -3145074.28075
+        self.assertAlmostEqual(ans, expected,places=5)
         
 
 if __name__ == '__main__':
